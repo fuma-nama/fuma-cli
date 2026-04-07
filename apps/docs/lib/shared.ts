6b11b5975abe2e13472e5b5d5bffed388270f14e
Fuma Nama <god63820869@gmail.com>
2026-04-07 18:34:02 +0800

    docs: init

---
+++ apps/docs/lib/shared.ts
@@ -0,0 +1,10 @@
+export const appName = "Fuma CLI";
+export const docsRoute = "/";
+export const docsImageRoute = "/og/docs";
+export const docsContentRoute = "/llms.mdx/docs";
+
+export const gitConfig = {
+  user: "fuma-nama",
+  repo: "fuma-cli",
+  branch: "main",
+};
